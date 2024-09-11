b06b0be42d4ac70142801c6101c9327ef67d4d06
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-11 14:34:18 +0100

    chore(root): add cjs and umd types

--- rollup.config.dts.mjs
+++ rollup.config.dts.mjs
@@ -7,9 +7,19 @@ const __dirname = dirname(__filename)
 
 export default {
   input: `${__dirname}/dist/types/src/index.d.ts`,
-  output: {
-    file: `${__dirname}/dist/index.d.ts`,
-    format: 'es',
-  },
+  output: [
+    {
+      file: `${__dirname}/dist/snaply.es.d.ts`, // ES module
+      format: 'es',
+    },
+    {
+      file: `${__dirname}/dist/snaply.cjs.d.ts`, // CommonJS module
+      format: 'cjs',
+    },
+    {
+      file: `${__dirname}/dist/snaply.umd.d.ts`, // UMD module
+      format: 'umd',
+    },
+  ],
   plugins: [dts()],
 }
