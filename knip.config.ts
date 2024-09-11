160b1d3d18008958be5aa1aec26a0ee574703c5c
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-11 16:04:25 +0100

    chore(root): release workflow

--- knip.config.ts
+++ knip.config.ts
@@ -5,7 +5,15 @@ const config: KnipConfig = {
     '.': {
       entry: ['src/**/*'],
       ignore: ['rollup.config.dts.mjs', 'tools/scripts/generate-contributors.mts'],
-      ignoreDependencies: ['@eslint/eslintrc', 'rollup-plugin-dts', '@nx/web', 'tsx'],
+      ignoreDependencies: [
+        '@eslint/eslintrc',
+        'rollup-plugin-dts',
+        '@nx/web',
+        'tsx',
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/github',
+        '@semantic-release/release-notes-generator',
+      ],
       ignoreBinaries: ['rollup'],
     },
   },
